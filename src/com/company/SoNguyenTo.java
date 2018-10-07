b6ed81551b193d653510c08db479d77cc14e906a
tinh1408 <tinhph@hotmail.com>
2018-10-07 22:27:39 +0700

    a Tính góp ý

--- src/com/company/SoNguyenTo.java
+++ src/com/company/SoNguyenTo.java
@@ -8,7 +8,7 @@ public class SoNguyenTo {
         int n;
         System.out.println("Nhập vào một số n = ");
         n = scanner.nextInt();
-        if(n>2)
+        if(n>2) // nếu anh nhập vào số 5 thì nó sẽ kiểm tra lớn hơn 2 và in ra là không phải số nguyên tố, vậy chưa đúng rồi, em sửa lại chỗ này nhé
         {
             System.out.println(n + "is not a prime");
         }
